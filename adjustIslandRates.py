2103a48c5440c7c6b7dce7516c0190793b626dd2
bud-aj29 <99773087+bud-aj29@users.noreply.github.com>
2023-11-17 08:03:43 +0900

    Removed dummy file
    Removed the dummy feature rule that was used to call static island at 0,0. Also changed island feature rules so no static island is created.
    
    Some players had problems with the pack not loading due to world.x fault. The dummy was the fix for that fault, but is sensitive to order of operation for different device OS. Removed this to improve reliability across devices.

--- adjustIslandRates.py
+++ adjustIslandRates.py
@@ -4,11 +4,10 @@ from glob import glob
 import const
 
 islandProbability=1.0#percent out of 100
-excludedFiles=["1_o-n_dummy_placement.json"]
 workingPack=const.workingPack
 
 def adjustProb(fileName):
-    scatter_chance = f"(variable.worldx == 0 && variable.worldz == 0) ? 100 : {islandProbability:.2f}"
+    scatter_chance = f"{islandProbability:.2f}"
     with open(fileName) as file:
         data = json.load(file)
     data["minecraft:feature_rules"]["distribution"]["scatter_chance"]=scatter_chance
@@ -16,5 +15,4 @@ def adjustProb(fileName):
         json.dump(data,file,indent=2)
 for file in glob(path.join(workingPack,"feature_rules","isl","*.json")):
     print(path.basename(file))
-    if path.basename(file) not in excludedFiles:
-        adjustProb(file)
+    adjustProb(file)
