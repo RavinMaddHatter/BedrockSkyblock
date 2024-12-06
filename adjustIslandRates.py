4b6d3a12122bc12537a6f08b54df329f8d76d638
bud-aj29 <99773087+bud-aj29@users.noreply.github.com>
2024-12-06 20:40:41 +0900

    1.21.50
    updated island spawn algorithm

--- adjustIslandRates.py
+++ adjustIslandRates.py
@@ -7,7 +7,8 @@ islandProbability=1.0#percent out of 100
 workingPack=const.workingPack
 
 def adjustProb(fileName):
-    scatter_chance = f"{islandProbability:.2f}"
+    #scatter_chance = f"{islandProbability:.2f}"
+    scatter_chance = "(math.mod((variable.originx/16)*(variable.originz/16),26)==1)?100:0"
     with open(fileName) as file:
         data = json.load(file)
     data["minecraft:feature_rules"]["distribution"]["scatter_chance"]=scatter_chance
